8bfb584127efdfa5a4738661a466ed906c4fee54
kthvty4 <ekavia618@gmail.com>
2024-12-04 13:57:49 +0700

    Codelab Modul 5 Laravel

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    //define properti
+    public $status;
+    public $message;
+    public $resource;
+
+    /**
+     * _construct
+     *
+     * @param mixed $status
+     * @param mixed $message
+     * @param mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource){
+        parent::__construct($resource);
+        $this->status = $status;
+        $this->message = $message;
+        $this->resource = $resource;
+    }
+
+    /**
+     * toArray
+     *
+     * @param mixed $request
+     * @return array
+     */
+    public function toArray(Request $request) : array {
+        return[
+            'success' => $this->status,
+            'message' => $this->message,
+            'data' => $this ->resource
+        ];
+    }
+}
